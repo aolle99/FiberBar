32b74856d9f19ef2c6f80cc410347e52347f4e33
Victor Pla Sanchis <vplasanchis@gmail.com>
2021-05-15 13:39:54 +0200

    Creació del activity de resum de compra

---
+++ app/src/main/java/cat/fiberfarra/fiberbar/PurchaseSummaryActivity.java
@@ -0,0 +1,14 @@
+package cat.fiberfarra.fiberbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PurchaseSummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_purchase_summary);
+    }
+}
