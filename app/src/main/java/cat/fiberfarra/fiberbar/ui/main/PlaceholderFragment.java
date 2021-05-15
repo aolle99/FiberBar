2c5d05c32620277e603dc5d662e2b718cdb36d0d
Alex Olle <aolle99@gmail.com>
2021-05-15 21:24:38 +0200

    carta final v1 ueueueue quiero yorar ayuda por favor, no puedo mas con mi vida, llevo demasiadas horas programando

--- app/src/main/java/cat/fiberfarra/fiberbar/ui/main/PlaceholderFragment.java
+++ app/src/main/java/cat/fiberfarra/fiberbar/ui/main/PlaceholderFragment.java
@@ -4,12 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import cat.fiberfarra.fiberbar.R;
@@ -53,8 +50,14 @@ public class PlaceholderFragment extends Fragment {
         if(index==1) {
             return inflater.inflate(R.layout.fragment_menu_view, null);
         }
+        else if(index==2){
+            return inflater.inflate(R.layout.fragment_menu_view2, null);
+        }
+        else if(index==3){
+            return inflater.inflate(R.layout.fragment_menu_view3, null);
+        }
         else {
-            return inflater.inflate(R.layout.fragment_menu_view, null);
+            return inflater.inflate(R.layout.fragment_menu_view4, null);
         }
 
     }
