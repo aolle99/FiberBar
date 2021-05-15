eb3f7dfa019915f07133c20cf1e3d9273bdb622b
Cristina Migo <Cristina@example.com>
2021-05-15 14:37:51 +0200

    activity de comanda feta

---
+++ app/src/main/java/cat/fiberfarra/fiberbar/ComandaFetaActivity.java
@@ -0,0 +1,14 @@
+package cat.fiberfarra.fiberbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComandaFetaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comanda_feta);
+    }
+}
